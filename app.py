ed3f59137a0fa9fecbdf1dc23356cf76bf642a9a
AKAD0 <52894844+AKAD0@users.noreply.github.com>
2025-04-08 01:56:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ def home():
     return "Hello!"
 
 if __name__ == "__main__":
-    app.run(host='192.168.1.208', port=8000) # "host='192.168.1.208:8000'" is gunicorn-nginx interface
+    app.run(host='192.168.1.208', port=8000) # Gunicorn-nginx interface
                                              # '192.168.1.208' is Raspberry's IP
                                              # '8000' specifies gunicorn-nginx port
